8982a0f8aa5e94a6f71274d1c896804bb21e0120
Lucas-benkendorf <benklucass@gmail.com>
2025-11-03 15:14:55 -0300

    feat: adicionado endpoints de Criação(Post) e Atualização (Put)

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, HTTPException
 from typing import List
-from models import Tarefa
-from database import tarefas
+from models import Tarefa, TarefaUpdate
+from database import tarefas, proximo_id
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/tarefas/", response_model=Tarefa)
+def criar_tarefa(tarefa: Tarefa):
+    global proximo_id
+    tarefas[proximo_id] = tarefa.model_dump()
+    tarefas[proximo_id]["id"] = proximo_id
+    proximo_id += 1
+    return tarefas[proximo_id - 1]
 
 @app.get("/tarefas/", response_model=List[Tarefa])
 def listar_tarefas():
@@ -18,3 +26,16 @@ def ler_tarefa(tarefa_id: int):
     if tarefa_id not in tarefas:
         raise HTTPException(status_code=404, detail="Tarefa não encontrada")
     return tarefas[tarefa_id]
+
+@app.put("/tarefas/{tarefa_id}", response_model=Tarefa)
+def atualizar_tarefa(tarefa_id: int, tarefa_update: TarefaUpdate):
+    if tarefa_id not in tarefas:
+        raise HTTPException(status_code=404, detail="Tarefa não encontrada")
+    
+    tarefa_existente = tarefas[tarefa_id]
+    update_data = tarefa_update.model_dump(exclude_unset=True)
+    
+    for key, value in update_data.items():
+        tarefa_existente[key] = value
+        
+    return tarefa_existente
