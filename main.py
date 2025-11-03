27770377a923ed8792e20e1487f5e39d8b553095
Lucas-benkendorf <benklucass@gmail.com>
2025-11-03 14:36:01 -0300

    feat: implementado endpoints de leitura (get)

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from typing import List
+from models import Tarefa
+from database import tarefas
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/tarefas/", response_model=List[Tarefa])
+def listar_tarefas():
+    return list(tarefas.values())
+
+@app.get("/tarefas/{tarefa_id}", response_model=Tarefa)
+def ler_tarefa(tarefa_id: int):
+    if tarefa_id not in tarefas:
+        raise HTTPException(status_code=404, detail="Tarefa não encontrada")
+    return tarefas[tarefa_id]
